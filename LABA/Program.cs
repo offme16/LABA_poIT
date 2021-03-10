b3e0a0ee6be21d6c8c483276a0a88e3125399af7
offme16 <offmemyg@gmail.com>
2021-03-11 01:36:18 +0300

    first commit

---
+++ LABA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LABA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            int count = 0;
+            while (k == 0)
+            {
+                if (Math.Pow(count, 3) > n)
+                {
+                    k = count;
+                }
+                else { count += 1; }
+            }
+            Console.WriteLine("K={0},3K={1}", k, k * 3);
+
+        }
+    }
+}
